2fa59ceba3d29a143d914e1faa06d10b95f73f53
Neil Seejoor <nvseejoor@gmail.com>
2018-07-22 19:03:36 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  resource :author
-  resource :post
+  resources :authors
+  resources :posts
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
